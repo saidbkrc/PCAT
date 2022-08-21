556f8fd87e01000f68fac5d2db08cf20845d0ac0
Said Büyükkaraca <saidbkrc14@gmail.com>
2022-08-21 21:49:45 +0300

    nodemon has been add

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-    console.log('Function Working');
-};
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    const photo = {
+        id: 1,
+        name: "Photo Name",
+        description: "Photo description"
+    }
+    res.send(photo);
+});
+
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda başlatıldı`);
+});
