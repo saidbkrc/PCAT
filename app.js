7856afc913049c6874442f8417eadf98a67e44fd
Said Büyükkaraca <saidbkrc14@gmail.com>
2022-08-22 11:39:02 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+
+//MIDDLEWARES
+app.use(express.static('public'));
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-    const photo = {
-        id: 1,
-        name: "Photo Name",
-        description: "Photo description"
-    }
-    res.send(photo);
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 app.listen(port, () => {
