9f30ca9093069de62a38a9e1ae1d04340aad0c18
Said Büyükkaraca <saidbkrc14@gmail.com>
2022-08-22 14:07:12 +0300

    Partials

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 const app = express();
+const port = 3000;
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
 
-const port = 3000;
-
+//ROUTES
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/add', (req, res) => {
+    res.render('add');
 });
 
 app.listen(port, () => {
